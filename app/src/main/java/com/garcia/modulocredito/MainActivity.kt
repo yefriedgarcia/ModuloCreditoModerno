eeba32f2451ca22189f9e05d6f2924240061d9d2
Hernan Garcia <68208221+yefriedgarcia@users.noreply.github.com>
2025-11-08 11:30:35 -0500

    [FEATURE] Primer pantalla CreditScreen (#1)
    * feat: se agrega nueva pantalla CreditScreen y VM
    
    * Clean code

--- app/src/main/java/com/garcia/modulocredito/MainActivity.kt
+++ app/src/main/java/com/garcia/modulocredito/MainActivity.kt
@@ -4,44 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.garcia.modulocredito.data.di.appModule
+import com.garcia.modulocredito.ui.CreditScreen
 import com.garcia.modulocredito.ui.theme.ModuloCreditoTheme
+import com.garcia.modulocredito.ui.viewmodel.CreditViewModel
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        startKoin { modules(appModule) }
         setContent {
             ModuloCreditoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val vm = koinViewModel<CreditViewModel>()
+                CreditScreen(vm)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ModuloCreditoTheme {
-        Greeting("Android")
     }
 }
