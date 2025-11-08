e22c107cda2d9556ad9d6e5490260fa88a0a8adc
Hernan Garcia <68208221+yefriedgarcia@users.noreply.github.com>
2025-11-08 12:39:07 -0500

    [FEATURE] Vista base y construción de lógica (#2)
    * feat: se agrega nueva pantalla CreditScreen y VM
    
    * Clean code
    
    * vista base performada, logica de viewmodel y usecase agregada
    
    * fix KoinApplication has not been started

--- app/src/main/java/com/garcia/modulocredito/data/di/AppModule.kt
+++ app/src/main/java/com/garcia/modulocredito/data/di/AppModule.kt
@@ -1,9 +1,14 @@
 package com.garcia.modulocredito.data.di
 
+import com.garcia.modulocredito.data.repository.CreditRepositoryImpl
+import com.garcia.modulocredito.domain.CreditRepository
+import com.garcia.modulocredito.domain.usecase.CalculateInstallmentUseCase
 import com.garcia.modulocredito.ui.viewmodel.CreditViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { CreditViewModel() }
+    single<CreditRepository> { CreditRepositoryImpl() }
+    single { CalculateInstallmentUseCase() }
+    viewModel { CreditViewModel(get(), get()) }
 }
