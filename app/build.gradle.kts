fb660073265738f91b8b9ccf00b809647cfcdac1
hernan <garciia.h.97@gmail.com>
2025-11-09 21:44:09 -0500

    chore: add test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    testImplementation(libs.turbine)
+    testImplementation(libs.truth)
+    testImplementation(kotlin("test"))
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(kotlin("test"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
